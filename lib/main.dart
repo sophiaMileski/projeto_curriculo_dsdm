09340e870a67fa7c4b9130fe297b5a136fe77975
Sophia Mileski <sophiamileskisantos@gmail.com>
2026-04-15 18:42:48 -0300

    class de TelaExperiencia criada, lista criada

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
 //usar widget catalog/materialcomponents/divider e floatingactionbutton
 void main(){
-  runApp (MaterialApp(home: TelaInicial()));
+  runApp (MaterialApp(home:TelaExperiencia()));
 }
   
-class TelaInicial extends StatelessWidget{
-  State<TelaInicial> createState() => _TelaInicialState();
-}
+class TelaExperiencia extends StatefulWidget{
+  @override
+  State<TelaExperiencia> createState()=> _TelaExperienciaState(); 
+   
+  }
+
+class _TelaExperienciaState extends State<TelaExperiencia> {
+  List<Experiencia> listaExp = [];
+
+  @override
+ Widget build(BuildContext context){
+  return Scaffold(
+    appBar: AppBar(
+      title: Text("Experiências"),
+    ),
+  )
+
+ }
+  }
+
+
 

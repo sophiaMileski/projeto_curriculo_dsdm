02de7d6c534a39758ccb1a51fd1a34c41dbb06c9
Sophia Mileski <sophiamileskisantos@gmail.com>
2026-04-16 17:03:53 -0300

    adicionei o circleavatar no card, criação da tela formulario

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,10 @@ class _TelaExperienciaState extends State<TelaExperiencia> {
           children: [
             Card(
               child: ListTile(
+                leading: CircleAvatar(
+                  backgroundImage: NetworkImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQc69QEfO57WGxQQgK2hv91wqQkMcXvefVOyg&s"
+                  ),
+                ),
                 title: Text(listaExp[index].empresa),
                 subtitle: Text(
                   "${listaExp[index].inicio} - ${listaExp[index].fim}",
@@ -48,12 +52,79 @@ class _TelaExperienciaState extends State<TelaExperiencia> {
     },
     ),
     floatingActionButton: FloatingActionButton(
-      onPressed: () {},
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => TelaFormulario()
+          ),).then((exp){
+            if (exp !=null){
+              setState(() {
+                listaExp.add(exp);
+              });
+            }
+          }
+          );
+      },
       child: Icon(Icons.add),
     ),
   );
   }
 }
+class TelaFormulario extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    TextEditingController empresa = TextEditingController();
+    TextEditingController inicio = TextEditingController();
+    TextEditingController fim= TextEditingController();
+
+    return Scaffold(
+      appBar: AppBar(title: Text("Nova Experiência")),
+      body: ListView(
+        padding: EdgeInsets.all(16),
+        children: [
+          Text('Empresa'),
+          TextField(controller:empresa),
+
+          SizedBox(height: 10),
+
+          Text('Ano início'),
+          TextField(controller:inicio),
+
+          SizedBox(height: 10),
+
+           Text('Ano fim'),
+          TextField(controller:fim),
+
+          SizedBox(height: 20),
+
+          ElevatedButton(
+            onPressed: (){
+              if (empresa.text != '' &&
+                  inicio.text != '' &&
+                  fim.text != ' &&'){
+                Experiencia exp = Experiencia(
+                  empresa.text,
+                  inicio.text,
+                  fim.text,
+                );
+
+                Navigator.pop(context, exp);
+              } else{
+                debugPrint("Preencha todos os campos, por favor");
+              }
+            },
+            child: Text("Salvar"),
+          ),
+        ],
+      ),
+    );
+    
+  }
+}
+
+
+
+
 class Experiencia {
   String empresa;
   String inicio;

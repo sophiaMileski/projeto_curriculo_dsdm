6145aa5ad24b20af9c206f0ebef7a4c77018a627
Sophia Mileski <sophiamileskisantos@gmail.com>
2026-04-20 20:45:34 -0300

    adicionei minha foto e mudei a tela inicial

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void  main(){
 
 }
 
-class TelaInicial extends StatelessWidget{
+class TelaInicial extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,47 +15,49 @@ class TelaInicial extends StatelessWidget{
         centerTitle: true,
         backgroundColor: Colors.pink,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          CircleAvatar(
-            radius: 60,
-            child: Icon(Icons.person, size: 60),
-          
-          ),
-          SizedBox(height: 30),
-
-          ElevatedButton(
-            onPressed:(){
-              Navigator.push(context,
-              MaterialPageRoute(builder: (_) => TelaExperiencia()));
-            },
-            child: Text("Experiência"),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 60,
+              backgroundImage: AssetImage('assets/foto.png'),
             ),
-
-           ElevatedButton(
-            onPressed:(){
-              Navigator.push(context,
-              MaterialPageRoute(builder: (_) => TelaProjetos()));
-            },
-            child: Text("Projetos"),
-            ),
-
+            SizedBox(height: 30),
             ElevatedButton(
-            onPressed:(){
-              Navigator.push(context,
-              MaterialPageRoute(builder: (_) => TelaEscolaridade()));
-            },
-            child: Text("Escolaridade"),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => TelaExperiencia()),
+                );
+              },
+              child: Text("Experiência"),
             ),
-        ],
-
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => TelaProjetos()),
+                );
+              },
+              child: Text("Projetos"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => TelaEscolaridade()),
+                );
+              },
+              child: Text("Escolaridade"),
+            ),
+          ],
+        ),
       ),
     );
-    
   }
 }
-
 class Experiencia{
   String empresa;
   String ano;
